05b98b00b0df36b819153959c7691eb6736096a1
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2023-07-01 17:19:45 +0530

    updated

--- gallery-react-tailwind/src/App.js
+++ gallery-react-tailwind/src/App.js
@@ -1,7 +1,41 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-6xl">Hello World</h1>
+    <div class="max-w-sm rounded overflow-hidden-shadow-lg">
+      <img
+        src="https://source.unsplash.com/random"
+        alt="img"
+        className="w-full"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by KC
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            40
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            300
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag3
+        </span>
+      </div>
     </div>
   );
 }
