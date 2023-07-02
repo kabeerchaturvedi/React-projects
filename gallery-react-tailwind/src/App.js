b0e554b539c30a609aeed567852c20490ccaf0e4
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2023-07-02 12:14:54 +0530

    updated cards

--- gallery-react-tailwind/src/App.js
+++ gallery-react-tailwind/src/App.js
@@ -1,40 +1,27 @@
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=38019168-aa3f7fc8a9a981982aff9eb51&q=${term}&image_type=photo&pretty=true`)
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
-    <div class="max-w-sm rounded overflow-hidden-shadow-lg">
-      <img
-        src="https://source.unsplash.com/random"
-        alt="img"
-        className="w-full"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by KC
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            40
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            300
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
