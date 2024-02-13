2455d8e00eb12d80c16c0ee2835cef3ff163df5e
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2024-02-13 22:05:14 +0530

    Basic Kanban Board Created with mock data

--- Notion/notion-app/src/App.js
+++ Notion/notion-app/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import DEFAULT_CARDS from "./data";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+      {" "}
+      <Board />
     </div>
   );
-}
+};
+
+const Board = () => {
+  const [cards, setCards] = useState(DEFAULT_CARDS);
+  return (
+    <div className="flex h-screen w-full gap-3 overflow-scroll p-12">
+      <Column title="Backlog" column="backlog" headingColor="text-neutral-500" cards={cards} setCards={setCards} />
+      <Column title="TODO" column="todo" headingColor="text-yellow-200" cards={cards} setCards={setCards} />
+      <Column title="In Progress" column="doing" headingColor="text-blue-200" cards={cards} setCards={setCards} />
+      <Column title="Complete" column="done" headingColor="text-emarald-200" cards={cards} setCards={setCards} />
+    </div>
+  );
+};
+
+const Column = ({ title, headingColor, column, cards, setCards }) => {
+  const [active, setActive] = useState(false);
+  const filteredCards = cards.filter((c) => c.column === column);
+  return (
+    <div className="w-56 shrink-0">
+      <div className="mb-3 flex items-center justify-between">
+        <h3 className={`font-medium ${headingColor}`}>{title}</h3>
+        <span className="rounded text-sm text-neutral-400">{filteredCards.length}</span>
+      </div>
+      <div className={`h-full w-full transition-colors ${active ? "bg-neutral-800/50" : "bg-neutral-800/0"} `}>
+        {filteredCards.map((c) => {
+          return <Card key={c.id} {...c} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Card = ({ title, id, column }) => {
+  <>
+    <div>
+      <p className="text-sm text-neutral-500">{title}</p>
+    </div>
+  </>;
+};
 
 export default App;
---
+++ Notion/notion-app/src/data.js
@@ -0,0 +1,25 @@
+const DEFAULT_CARDS = [
+  //Backlog
+  { title: "Learn React", id: 1, column: "backlog" },
+  { title: "Redux Documentation", id: 2, column: "backlog" },
+  { title: "Migrate to Azure with help of Docker", id: 3, column: "backlog" },
+  { title: "Prepare Timelines", id: 4, column: "backlog" },
+
+  //TODO
+  { title: "React Tutorial", id: 5, column: "todo" },
+  { title: "Postportem for outage", id: 6, column: "todo" },
+  { title: "Framer Motion & Animations", id: 7, column: "todo" },
+
+  //Doing
+
+  { title: "React & Redux Tutorial", id: 8, column: "doing" },
+  { title: "CodeDamn Tutorial", id: 9, column: "doing" },
+  { title: "Harkirat Vercel Video", id: 10, column: "doing" },
+  { title: "Indians on opensource", id: 11, column: "doing" },
+
+  //done
+  { title: "Akshay Saini Tutorial", id: 12, column: "done" },
+  { title: "Redux Tutorial ", id: 13, column: "done" },
+];
+
+export default DEFAULT_CARDS;
