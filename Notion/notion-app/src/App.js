7c233464a7c538b27573af1a27e92b525dce074b
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2024-02-19 12:44:13 +0530

    Setup form completed and deletion created

--- Notion/notion-app/src/App.js
+++ Notion/notion-app/src/App.js
@@ -61,16 +61,45 @@ const Column = ({ title, headingColor, column, cards, setCards }) => {
 const AddCard = ({ column, setCards }) => {
   const [text, setText] = useState("");
   const [adding, setAdding] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text.trim().length) return;
+
+    const newCard = {
+      column,
+      title: text.trim(),
+      id: Math.random().toString(),
+    };
+
+    setCards((pv) => [...pv, newCard]);
+  };
   return (
     <>
       {adding ? (
-        <form>
+        <form onSubmit={handleSubmit}>
           <textarea
             onChange={(e) => setText(e.target.value)}
             autoFocus
             placeholder="Add new Task"
             className="w-full rounded border border-violet-400 bg-violet-400/20 p-3 text-sm text-neutral-50 placeholder-violet-300 focus:outline-0"
           />
+          <div className="mt-1.5 flex items-center justify-end gap-1.5">
+            <button
+              onClick={() => setAdding(false)}
+              className="px-3 py-1.5 text-xs text-neutral-400 transition-colors hover:text-neutral-50"
+            >
+              Close
+            </button>
+            <button
+              type="submit"
+              className="flex items-center gap-1.5 rounded bg-neutral-50 px-3 p-1.5 text-xs text-neutral-950 transition-colors hover:bg-neutral-300"
+            >
+              <span>Add</span>
+              <FiPlus />
+            </button>
+          </div>
         </form>
       ) : (
         <button
