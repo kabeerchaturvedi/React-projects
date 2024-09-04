006c5686bd42bca95c958d7ff52cd2f46f2e3805
kabeerchaturvedi <kbrchaturvedi@gmail.com>
2024-09-04 14:28:54 +0530

    Create App.js

---
+++ Calendar/App.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Dayview from "./components/Tabs";
+
+function App() {
+  return (
+    <div>
+      <Dayview />
+    </div>
+  );
+}
+
+export default App;
