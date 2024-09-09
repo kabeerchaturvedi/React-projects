a9c9ad17aa9720e212b54b21a398e4cd5d5c2a97
kabeerchaturvedi <kbrchaturvedi@gmail.com>
2024-09-09 17:21:16 +0530

    Create App.js

---
+++ Traffic Lights/App.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Traffic from "./components/Events";
+
+function App() {
+  return (
+    <div>
+      <Traffic />
+    </div>
+  );
+}
+
+export default App;
